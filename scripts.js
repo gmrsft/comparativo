5c29c62c217666fdc2202a10d3864ed983baa7f7
Gui <admguimat@gmail.com>
2022-06-03 13:58:32 -0300

    novas funcoes para emular em terminal

--- scripts.js
+++ scripts.js
@@ -1,26 +1,31 @@
-function num (){
-    let x = Number(prompt('Digite um valor: '));
-    let y = Number(prompt('Digite um valor: '));
-    let cont = x + y;
+function num (x, y){
+    // let x = Number(prompt('Digite um valor: '));
+    // let y = Number(prompt('Digite um valor: '));
+    const cont = x + y;
 
     function igual (){
         if (x === y){
-        alert(`Os números ${x} e ${y} são iguais.`)
+        return(`Os números ${x} e ${y} são iguais.`)
         } else{
-            alert(`Os números ${x} e ${y} não são iguais.`)
+            return(`Os números ${x} e ${y} não são iguais.`)
         }
-    } igual()
+    } 
 
     function soma(){
         if (x + y > 10 && x + y < 20){
-            alert(` Sua soma é ${cont}, que é maior que 10 e menor que 20.`)
+            return(` Sua soma é ${cont}, que é maior que 10 e menor que 20.`)
         }else if (x + y > 10){
-            alert(` Sua soma é ${cont}, que é maior que 10 e maior que 20.`)
+            return(` Sua soma é ${cont}, que é maior que 10 e maior que 20.`)
         }else{
-            alert(` Sua soma é ${cont}, que é menor que 10 e menor que 20.`)
+            return(` Sua soma é ${cont}, que é menor que 10 e menor que 20.`)
         }
        
-    } soma()     
+    }
+
+    return `${igual()}${soma()}`;
+
+} 
+
+console.log(num(1,1));
 
 
-} num()
